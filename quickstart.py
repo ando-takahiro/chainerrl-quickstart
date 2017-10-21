1e26b9fb24284f04b4e4549957af5c72695ce1a1
ando-takahiro <takahiro.ando@gmail.com>
2017-10-21 20:51:21 +0900

    Experimental distributed learning support

--- quickstart.py
+++ quickstart.py
@@ -1,33 +1,31 @@
+import argparse
+import os
+import logging
+
 import chainer
 import chainer.functions as F
 import chainer.links as L
 import chainerrl
+from chainerrl.agents import a3c
+from chainerrl import experiments
+from chainerrl import misc
+from chainerrl.optimizers.nonbias_weight_decay import NonbiasWeightDecay
+from chainerrl.optimizers import rmsprop_async
+from chainerrl import policy
+from chainerrl.recurrent import RecurrentChainMixin
+from chainerrl import v_function
 import gym
 import numpy as np
 
-env = gym.make('CartPole-v0')
-print('observation space:', env.observation_space)
-print('action space:', env.action_space)
+class QHead(chainer.Chain):
 
-obs = env.reset()
-env.render()
-print('initial observation:', obs)
-
-action = env.action_space.sample()
-obs, r, done, info = env.step(action)
-print('next observation:', obs)
-print('reward:', r)
-print('done:', done)
-print('info:', info)
-
-class QFunction(chainer.Chain):
-
-    def __init__(self, obs_size, n_actions, n_hidden_channels=50):
+    def __init__(self, obs_size, n_hidden_channels=50):
         super().__init__()
         with self.init_scope():
             self.l0 = L.Linear(obs_size, n_hidden_channels)
             self.l1 = L.Linear(n_hidden_channels, n_hidden_channels)
-            self.l2 = L.Linear(n_hidden_channels, n_actions)
+            # self.l2 = L.Linear(n_hidden_channels, n_actions)
+        self.n_output_channels = n_hidden_channels
 
     def __call__(self, x, test=False):
         """
@@ -37,95 +35,174 @@ class QFunction(chainer.Chain):
         """
         h = F.tanh(self.l0(x))
         h = F.tanh(self.l1(h))
-        return chainerrl.action_value.DiscreteActionValue(self.l2(h))
+        return h #self.l2(h)
 
-obs_size = env.observation_space.shape[0]
-n_actions = env.action_space.n
-q_func = QFunction(obs_size, n_actions)
+class A3CFF(chainer.ChainList, a3c.A3CModel):
 
-q_func = chainerrl.q_functions.FCStateQFunctionWithDiscreteAction(
-    obs_size, n_actions,
-    n_hidden_layers=2, n_hidden_channels=50)
+    def __init__(self, obs_size, n_actions):
+        self.head = QHead(obs_size)
+        self.pi = policy.FCSoftmaxPolicy(
+            self.head.n_output_channels, n_actions)
+        self.v = v_function.FCVFunction(self.head.n_output_channels)
+        super().__init__(self.head, self.pi, self.v)
 
-# Use Adam to optimize q_func. eps=1e-2 is for stability.
-optimizer = chainer.optimizers.Adam(eps=1e-2)
-optimizer.setup(q_func)
+    def pi_and_v(self, state):
+        out = self.head(state)
+        return self.pi(out), self.v(out)
 
-# Set the discount factor that discounts future rewards.
-gamma = 0.95
 
-# Use epsilon-greedy for exploration
-explorer = chainerrl.explorers.ConstantEpsilonGreedy(
-    epsilon=0.3, random_action_func=env.action_space.sample)
+class A3CLSTM(chainer.ChainList, a3c.A3CModel, RecurrentChainMixin):
 
-# DQN uses Experience Replay.
-# Specify a replay buffer and its capacity.
-replay_buffer = chainerrl.replay_buffer.ReplayBuffer(capacity=10 ** 6)
+    def __init__(self, obs_size, n_actions):
+        self.head = QHead(obs_size)
+        self.pi = policy.FCSoftmaxPolicy(
+            self.head.n_output_channels, n_actions)
+        self.v = v_function.FCVFunction(self.head.n_output_channels)
+        self.lstm = L.LSTM(self.head.n_output_channels,
+                           self.head.n_output_channels)
+        super().__init__(self.head, self.lstm, self.pi, self.v)
 
-# Since observations from CartPole-v0 is numpy.float64 while
-# Chainer only accepts numpy.float32 by default, specify
-# a converter as a feature extractor function phi.
-phi = lambda x: x.astype(np.float32, copy=False)
+    def pi_and_v(self, state):
+        h = self.head(state)
+        h = self.lstm(h)
+        return self.pi(h), self.v(h)
 
-# Now create an agent that will interact with the environment.
-agent = chainerrl.agents.DoubleDQN(
-    q_func, optimizer, replay_buffer, gamma, explorer,
-    replay_start_size=500, update_interval=1,
-    target_update_interval=100, phi=phi)
 
-# Set up the logger to print info messages for understandability.
-import logging
-import sys
-gym.undo_logger_setup()  # Turn off gym's default logger settings
-logging.basicConfig(level=logging.INFO, stream=sys.stdout, format='')
+def main():
 
-chainerrl.experiments.train_agent_with_evaluation(
-    agent, env,
-    steps=2000,           # Train the agent for 2000 steps
-    eval_n_runs=10,       # 10 episodes are sampled for each evaluation
-    max_episode_len=200,  # Maximum length of each episodes
-    eval_interval=1000,   # Evaluate the agent after every 1000 steps
-    outdir='result')      # Save everything to 'result' directory
-#n_episodes = 200
-#max_episode_len = 200
-#for i in range(1, n_episodes + 1):
-#    obs = env.reset()
-#    reward = 0
-#    done = False
-#    R = 0  # return (sum of rewards)
-#    t = 0  # time step
-#    while not done and t < max_episode_len:
-#        # Uncomment to watch the behaviour
-#        # env.render()
-#        action = agent.act_and_train(obs, reward)
-#        obs, reward, done, _ = env.step(action)
-#        R += reward
-#        t += 1
-#    if i % 10 == 0:
-#        print('episode:', i,
-#              'R:', R,
-#              'statistics:', agent.get_statistics())
-#    agent.stop_episode_and_train(obs, reward, done)
+    # Prevent numpy from using multiple threads
+    os.environ['OMP_NUM_THREADS'] = '1'
 
-print('Finished.')
+    logger = logging.getLogger(__file__)
+    formatter = logging.Formatter(
+        fmt="%(asctime)s:[%(levelname)s] %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
+    )
+    logger.setLevel(logging.DEBUG)
+    logger.propagate = False
 
-for i in range(10):
-    obs = env.reset()
-    done = False
-    R = 0
-    t = 0
-    while not done and t < 200:
-        env.render()
-        action = agent.act(obs)
-        obs, r, done, _ = env.step(action)
-        R += r
-        t += 1
-    print('test episode:', i, 'R:', R)
-    agent.stop_episode()
+    # output to stdout
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    stream_handler.setLevel(logging.DEBUG)
+    logger.addHandler(stream_handler)
+    # logging.basicConfig(level=logging.DEBUG, stream=sys.stdout, format='')
+    logger.info('hello')
 
-# Save an agent to the 'agent' directory
-agent.save('agent')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('processes', type=int)
+    parser.add_argument('--seed', type=int, default=None)
+    parser.add_argument('--outdir', type=str, default=None)
+    parser.add_argument('--use-sdl', action='store_true')
+    parser.add_argument('--t-max', type=int, default=5)
+    parser.add_argument('--max-episode-len', type=int, default=200)#10000)
+    parser.add_argument('--beta', type=float, default=1e-2)
+    parser.add_argument('--profile', action='store_true')
+    parser.add_argument('--steps', type=int, default=2000) #8 * 10 ** 7)
+    parser.add_argument('--lr', type=float, default=7e-4)
+    parser.add_argument('--eval-interval', type=int, default=1000) #10 ** 6)
+    parser.add_argument('--eval-n-runs', type=int, default=10)
+    parser.add_argument('--weight-decay', type=float, default=0.0)
+    parser.add_argument('--use-lstm', action='store_true')
+    parser.add_argument('--demo', action='store_true', default=False)
+    parser.add_argument('--load', type=str, default='')
+    parser.add_argument('--save', type=str, default='')
+    parser.set_defaults(use_sdl=False)
+    parser.set_defaults(use_lstm=False)
+    args = parser.parse_args()
 
-# Uncomment to load an agent from the 'agent' directory
-# agent.load('agent')
+    if args.seed is not None:
+        misc.set_random_seed(args.seed)
+
+    args.outdir = experiments.prepare_output_dir(args, args.outdir)
+
+    print('Output files are saved in {}'.format(args.outdir))
+
+    env = gym.make('CartPole-v0')
+    obs_size = env.observation_space.shape[0]
+    n_actions = env.action_space.n
+
+    if args.use_lstm:
+        model = A3CLSTM(obs_size=obs_size, n_actions=n_actions)
+    else:
+        model = A3CFF(obs_size=obs_size, n_actions=n_actions)
+
+    opt = rmsprop_async.RMSpropAsync(lr=7e-4, eps=1e-1, alpha=0.99)
+    opt.setup(model)
+    opt.add_hook(chainer.optimizer.GradientClipping(40))
+
+    if args.weight_decay > 0:
+        opt.add_hook(NonbiasWeightDecay(args.weight_decay))
+
+    # Since observations from CartPole-v0 is numpy.float64 while
+    # Chainer only accepts numpy.float32 by default, specify
+    # a converter as a feature extractor function phi.
+    phi = lambda x: x.astype(np.float32, copy=False)
+
+    agent = a3c.A3C(model, opt, t_max=args.t_max, gamma=0.99,
+                    beta=args.beta, phi=phi)
+    if args.load:
+        agent.load(args.load)
+
+    def make_env(process_idx, test):
+        env = gym.make('CartPole-v0')
+        if not test:
+            chainerrl.misc.env_modifiers.make_reward_clipped(env, -1, 1)
+
+        return env
+
+    if args.demo:
+        env = make_env(0, True)
+        eval_stats = experiments.eval_performance(
+            env=env,
+            agent=agent,
+            n_runs=args.eval_n_runs)
+        print('n_runs: {} mean: {} median: {} stdev: {}'.format(
+            args.eval_n_runs, eval_stats['mean'], eval_stats['median'],
+            eval_stats['stdev']))
+    else:
+
+        # Linearly decay the learning rate to zero
+        def lr_setter(env, agent, value):
+            agent.optimizer.lr = value
+
+        lr_decay_hook = experiments.LinearInterpolationHook(
+            args.steps, args.lr, 0, lr_setter)
+
+        experiments.train_agent_async(
+            agent=agent,
+            outdir=args.outdir,
+            processes=args.processes,
+            make_env=make_env,
+            profile=args.profile,
+            steps=args.steps,
+            eval_n_runs=args.eval_n_runs,
+            eval_interval=args.eval_interval,
+            max_episode_len=args.max_episode_len,
+            global_step_hooks=[lr_decay_hook],
+            logger=logger
+        )
+
+        # Save an agent to the 'agent' directory
+        if args.save:
+            agent.save(args.save)
+
+
+    print('Finished.')
+
+    for i in range(10):
+        obs = env.reset()
+        done = False
+        R = 0
+        t = 0
+        while not done and t < 200:
+            env.render()
+            action = agent.act(obs)
+            obs, r, done, _ = env.step(action)
+            R += r
+            t += 1
+        print('test episode:', i, 'R:', R, 't:', t, 'done:', done)
+        agent.stop_episode()
+
+if __name__ == '__main__':
+    main()
 
